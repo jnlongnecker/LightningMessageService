750c6e06060928647fc05e148bca5e215b07421a
jnlongnecker <jlnecker1@gmail.com>
2022-05-27 10:58:29 -0400

    Comments

--- force-app/main/default/aura/AuraMessageService/AuraMessageServiceHelper.js
+++ force-app/main/default/aura/AuraMessageService/AuraMessageServiceHelper.js
@@ -1,13 +1,18 @@
 ({
+    // This function handles a publish to the channel and saves the contents
     storeMessage: function (component, event) {
         if (event == null) return;
 
+        // Note that the event object contains the channel message
         component.set("v.message", event.getParam("myMessage"));
         component.set("v.from", event.getParam("from"));
     },
 
+    // Posts a static message to the message channel
     postMessage: function (component) {
         const data = { myMessage: 'Aura is cool!', from: 'Aura Component' };
+
+        // Note we get a reference to the message channel component and call the publish() method on it
         component.find("msgChannel").publish(data);
     }
 })
--- force-app/main/default/lwc/messageService/messageService.js
+++ force-app/main/default/lwc/messageService/messageService.js
@@ -1,5 +1,12 @@
+/*
+    Created by: Jared Longnecker
+    Last modified: 5/27/2022
+    Description: Web Component to showcase communication between UI frameworks using LMS
+*/
+
 import { LightningElement, wire } from 'lwc';
 
+// Note the import of the MessageContext and the Message Channel
 import myChannel from '@salesforce/messageChannel/myChannel__c';
 import { subscribe, publish, MessageContext } from 'lightning/messageService';
 
@@ -10,17 +17,21 @@ export default class MessageService extends LightningElement {
     messageServiceMessage = "No Messages Yet";
     messageCameFrom = "No Messages Yet";
 
+    // Subscription can happen anywhere, but it's convenient to subscribe ASAP
     connectedCallback() {
         this.subscription = subscribe(
             this.context, myChannel, (message) => this.messagePosted(message)
         );
     }
 
+    // Callback to run whenever a message gets posted to store the message contents
     messagePosted(message) {
         this.messageServiceMessage = message.myMessage;
         this.messageCameFrom = message.from;
     }
 
+    // Post a static message to the message channel. Note this can be non-static, but be careful
+    // about posting non-primitives! Those are passed by reference
     postMessage() {
         const data = { myMessage: 'LWC is fun!', from: 'Web Component' };
         publish(this.context, myChannel, data);
